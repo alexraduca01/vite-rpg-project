9de26ba1fecec889fe6f7b8f8d6d0a84d2695c33
alexraduca01 <alex.raduca01@icloud.com>
2024-01-23 15:38:18 +0100

    did installations

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <AppHome />
+  </div>
+</template>
+
+<script>
+import AppHome from './pages/AppHome.vue';
+  export default {
+    name: 'App',
+    components: {
+      AppHome,
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/pages/AppHome.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1 class="text-center">Home <i class="fa-solid fa-house"></i></h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AppHome',
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
