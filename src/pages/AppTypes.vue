bc9446564be3abef277bb1ecb41a096bf80d8672
federicomanni97 <manni_federico@libero.it>
2024-01-23 16:30:56 +0100

    app types + type detail

--- src/pages/AppTypes.vue
+++ src/pages/AppTypes.vue
@@ -1,12 +1,28 @@
 <template>
-    <div>
-
+   <div class="card" style="width: 18rem;" v-for="type in types" :key="type.id">
+        <div class="card-body">
+            <h5 class="card-title">{{ type.name }}</h5>
+            <p class="card-text">{{ type.description }}</p>
+            <p class="card-text my-4 fs-2">
+                // <router-link :to="{ name: 'TypesDetail', params: { id: type.id }}" class="btn btn-primary fs-3 mb-3" >
+                //     {{ type.name }}
+                // </router-link>
+            </p>
+        </div>
     </div>
 </template>
 
 <script>
+import {store} from '../store';
+import axios from 'axios';
     export default {
         name: 'AppTypes',
+        data(){
+            return {
+                store,
+                types: [],
+            }
+        },
     }
 </script>
 
---
+++ src/pages/TypeDetail.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">{{ type.name }}</h5>
+            <p class="card-text">{{ type.description }}</p>
+            <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+    export default {
+        name: 'TypeDetail',
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
