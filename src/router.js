4556d9d09c6f8ced57dc3e63c7829ff9554302e2
alexraduca01 <alex.raduca01@icloud.com>
2024-01-23 15:48:50 +0100

    added routes on router.js

--- src/router.js
+++ src/router.js
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AppHome from './pages/AppHome.vue'
+import AppCharacters from './pages/AppCharacters.vue'
+import AppItems from './pages/AppItems.vue'
+import AppTypes from './pages/AppTypes.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +12,21 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: AppHome
+    },
+    {
+      path: '/characters',
+      name: 'characters',
+      component: AppCharacters
+    },
+    {
+      path: '/items',
+      name: 'items',
+      component: AppItems
+    },
+    {
+      path: '/types',
+      name: 'types',
+      component: AppTypes
     },
   ]
 })
