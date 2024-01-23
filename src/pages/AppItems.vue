ff4f41d38ee6791f063bfc170c541bc3f88be300
AugustoM94 <email@github.com>
2024-01-23 16:27:44 +0100

    create Item app e detail

---
+++ src/pages/AppItems.vue
@@ -0,0 +1,31 @@
+<template>
+    <main class="container">
+
+    </main>
+</template>
+
+<script>
+import { store } from "../store";
+import axios from "axios";
+import AppItems from '../components/AppItems.vue';
+
+
+export default {
+    name: 'AppItems',
+    components: {
+        AppItems
+    },
+    data() {
+        return {
+            store,
+        }
+    },
+    methods: {
+      
+    },
+    created() {
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
