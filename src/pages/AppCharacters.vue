bf6beebcb17e7324f71e14c8bfb5a4278052eb59
DavideGila <davide.gila@gmail.com>
2024-01-23 16:38:51 +0100

    made a placeholder for characters index

--- src/pages/AppCharacters.vue
+++ src/pages/AppCharacters.vue
@@ -1,12 +1,48 @@
 <template>
-    <div>
+    <div class="container">
+        <div class="row">
+            <div v-for="character in characters" class="col-4">
+                <img :src="store.imgBasePath + character.image" :alt="character.name">
+                <div>
+                    <h3>{{ character.name }}</h3>
+                    <p>{{ character.description }}</p>
 
+                    <h4>Stats</h4>
+                    <ul class="list-unstyled">
+                        <li>Attack: {{ character.attack }}</li>
+                        <li>Defense: {{ character.defence }}</li>
+                        <li>Health: {{ character.life }}</li>
+                        <li>Speed: {{ character.speed }}</li>
+                        <li>Sex: {{ character.sex }}</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+import { store } from '../store';
     export default {
         name: 'AppCharacters',
+        data() {
+            return {
+                store,
+                characters: [],
+            }
+        },
+        methods: {
+            getAllCharacters(){
+                axios.get(store.apiUrl + '/characters').then((res) => {
+                    console.log(res.data);
+                    this.characters = res.data.results;
+                })
+            }
+        },
+        mounted(){
+            this.getAllCharacters();
+        }
     }
 </script>
 
