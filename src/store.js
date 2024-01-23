9aab239fb0cefd3a02911cd5e9a35667089a3323
federicomanni97 <manni_federico@libero.it>
2024-01-23 17:18:54 +0100

    store changes

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { reactive } from 'vue';
 
 export const store = reactive ({
-    apiUrl: "http://localhost:8001/api/",
-    imgBasePath: "http://localhost:8001/storage/",
+    apiUrl: "http://localhost:8000/api/",
+    imgBasePath: "http://localhost:8000/storage/",
     urlPagination: "?page=",
 })
