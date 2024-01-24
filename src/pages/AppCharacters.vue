779892e3a27d1ee2d7d93dbf505e023a026e125a
DavideGila <davide.gila@gmail.com>
2024-01-24 16:57:03 +0100

    added props

--- src/pages/AppCharacters.vue
+++ src/pages/AppCharacters.vue
@@ -1,32 +1,24 @@
 <template>
+    <h1>Characters</h1>
     <div class="container">
         <div class="row">
-            <div v-for="character in characters" class="col-6">
-                <img :src="store.imgBasePath + character.image" :alt="character.name">
-                <div>
-                    <h3>{{ character.name }}</h3>
-                    <p>{{ character.description }}</p>
-
-                    <h4>Stats</h4>
-                    <ul class="list-unstyled">
-                        <li>Attack: {{ character.attack }}</li>
-                        <li>Defense: {{ character.defence }}</li>
-                        <li>Health: {{ character.life }}</li>
-                        <li>Speed: {{ character.speed }}</li>
-                        <li>Sex: {{ character.sex }}</li>
-                    </ul>
-                </div>
-                <button><router-link :to="{ name: 'character-detail', params: { id: character.id } }">View</router-link></button>
+            <div v-for="character in characters" class="col-4">
+                <AppCard :character="character" />
             </div>
         </div>
     </div>
+       
 </template>
 
 <script>
 import axios from 'axios';
 import { store } from '../store';
+import AppCard from '../components/AppCard.vue';
     export default {
         name: 'AppCharacters',
+        components: {
+            AppCard
+        },
         data() {
             return {
                 store,
