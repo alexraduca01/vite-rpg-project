399ac7098efe764dfed4d11ff2921e00e493c24f
alexraduca01 <alex.raduca01@icloud.com>
2024-01-27 12:04:51 +0100

    edited AppHome & store.js

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,18 +1,35 @@
 <template>
-    <div>
-        <h1 class="text-center">Home <i class="fa-solid fa-house"></i></h1>
+    <div class="h-100">
+        <div class="w-100">
+            <video class="w-100" autoplay loop>
+                <source src="/videos/wallpaper.mp4" type="video/mp4">
+            </video>
+        </div>
+        <div class="container">
+            <div class="w-100">
+                <video class="w-100" autoplay loop>
+                    <source :src="store.videos[getRndInteger(0, store.videos.length - 1)].path" type="video/mp4">
+                </video>
+                <h2></h2>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-
+import { store } from '../store'
     export default {
         name: 'AppHome',
         
         data() {
             return {
-
+                store,
             }
+        },
+        methods: {
+            getRndInteger(min, max) {
+                return Math.floor(Math.random() * (max - min + 1) ) + min;
+            },
         }
     }
 </script>
--- src/store.js
+++ src/store.js
@@ -6,4 +6,18 @@ export const store = reactive ({
     urlPagination: "?page=",
     characters: [],
     iaCharacters: [],
+    videos: [
+        {
+            path: "/public/videos/skyrim.mp4",
+            name: "Skyrim",
+        },
+        {
+            path: "/public/videos/baldurs-gate.mp4",
+            name: "Baldrus Gate 3",
+        },
+        {
+            path: "/public/videos/witcher.mp4",
+            name: "The Witcher 3: Wild Hunt",
+        },
+    ],
 })
