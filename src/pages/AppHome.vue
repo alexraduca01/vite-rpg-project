8e7b42bdab150d2ed362c82bc6f461e648ba25de
alexraduca01 <alex.raduca01@icloud.com>
2024-01-27 13:42:48 +0100

    added frames & edited some style

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,16 +1,40 @@
 <template>
-    <div class="h-100">
-        <div class="w-100">
-            <video class="w-100" autoplay loop>
-                <source src="/videos/wallpaper.mp4" type="video/mp4">
-            </video>
+    <div class="h-100 bgsmoke">
+        <div class="w-100" style="height: 1430px;">
+            <video class="w-100 back-video" src="/videos/elden-ring.mp4" autoplay muted loop></video>
         </div>
-        <div class="container">
-            <div class="w-100">
-                <video class="w-100" autoplay loop>
-                    <source :src="store.videos[getRndInteger(0, store.videos.length - 1)].path" type="video/mp4">
-                </video>
-                <h2></h2>
+        <div class="py-5 w-100">
+            <div class="container">
+                <div class="w-100 d-flex justify-content-between gap-5 pb-5 mb-5">
+                    <img src="/public/img/cornice.png" class="frame" alt="cornice">
+                    <video class="w-65" autoplay loop muted>
+                        <source :src="store.videos[0].path" type="video/mp4">
+                    </video>
+                    <div class="text-white d-flex flex-column justify-content-center">
+                        <h2>{{ store.videos[0].name }}</h2>
+                        <p>{{ store.videos[0].desc }}</p>
+                    </div>
+                </div>
+                <div class="w-100 d-flex justify-content-between gap-5 pb-5 mb-5 position-relative">
+                    <img src="/public/img/cornice.png" class="frame frame1" alt="cornice">
+                    <div class="text-white text-center d-flex flex-column justify-content-center">
+                        <h3 class="fs-2" >{{ store.videos[1].name }}</h3>
+                        <p>{{ store.videos[1].desc }}</p>
+                    </div>
+                    <video class="w-65" autoplay loop muted>
+                        <source :src="store.videos[1].path" type="video/mp4">
+                    </video>
+                </div>
+                <div class="w-100 d-flex justify-content-between gap-5 pb-5 mb-5">
+                    <img src="/public/img/cornice.png" class="frame" alt="cornice">
+                    <video class="w-65" autoplay loop muted>
+                        <source :src="store.videos[2].path" type="video/mp4">
+                    </video>
+                    <div class="text-white text-center d-flex flex-column justify-content-center">
+                        <h2>{{ store.videos[2].name }}</h2>
+                        <p>{{ store.videos[2].desc }}</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -30,10 +54,43 @@ import { store } from '../store'
             getRndInteger(min, max) {
                 return Math.floor(Math.random() * (max - min + 1) ) + min;
             },
-        }
+            getRandomVideo() {
+                this.randomVideo = store.videos[this.getRndInteger(0, store.videos.length - 1)];
+            }
+        },
     }
 </script>
 
 <style lang="scss" scoped>
 
+.w-65 {
+    width: 65%;
+}
+
+.bgsmoke{
+    background-attachment: fixed;
+    background-size: cover;
+    background-image: url('https://baldursgate3.game/masks/smoke.jpg');
+}
+
+.back-video {
+    position: absolute;
+    top: 0;
+    left: 0;
+}
+
+.frame{
+    position: absolute;
+    width: 842px;
+}
+
+.frame1 {
+    right: 0;
+}
+
+h2 {
+    transform: translateX(-85px);
+    background-color: gold;
+    background-size: cover;
+}
 </style>
--- src/store.js
+++ src/store.js
@@ -10,14 +10,17 @@ export const store = reactive ({
         {
             path: "/public/videos/skyrim.mp4",
             name: "Skyrim",
+            desc: " The Elder Scrolls V: Skyrim è un videogioco di ruolo open-world sviluppato da Bethesda Game Studios, rilasciato nel 2011. Fa parte della serie The Elder Scrolls ed è ambientato nella provincia immaginaria di Skyrim, all'interno del continente di Tamriel. "
         },
         {
             path: "/public/videos/baldurs-gate.mp4",
-            name: "Baldrus Gate 3",
+            name: "Baldurs Gate 3",
+            desc: " Baldur\'s Gate 3 è un videogioco di ruolo (RPG) sviluppato da Larian Studios, basato sul popolare franchise di Dungeons & Dragons. Il gioco è stato rilasciato in accesso anticipato nel 2020. Si tratta di un sequel spirituale della serie Baldur\'s Gate, ambientato nell\'universo fantasy di Forgotten Realms. "
         },
         {
             path: "/public/videos/witcher.mp4",
             name: "The Witcher 3: Wild Hunt",
+            desc: " The Witcher 3: Wild Hunt è un videogioco di ruolo action-adventure sviluppato da CD Projekt. Rilasciato nel 2015, è il terzo capitolo della serie di giochi The Witcher, basata sulla serie di libri dello scrittore polacco Andrzej Sapkowski."
         },
     ],
 })
